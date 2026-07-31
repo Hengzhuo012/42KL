29df75d3aecce177af63a5742d27f01190614142
zheng <zheng@student.42kl.edu.my>
2026-07-31 17:44:08 +0800

    Milestone 1

---
+++ Core/Milestone 1/ft_printf/Before/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zheng <zheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/31 15:23:53 by zheng             #+#    #+#             */
+/*   Updated: 2026/07/31 16:55:27 by zheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unsitd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
