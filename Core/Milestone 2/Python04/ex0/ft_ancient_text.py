b781123747d4fec2fdffcb37536ebdc5fe76dd4f
zheng <zheng@student.42kl.edu.my>
2026-09-24 02:06:30 +0800

    started python04

--- Core/Milestone 2/Python04/ex0/ft_ancient_text.py
+++ Core/Milestone 2/Python04/ex0/ft_ancient_text.py
@@ -8,7 +8,7 @@ import typing
 
 def ft_acient_text():
     try:
-        file_handle = open(sys.argv[1])
+        file_handle = open(sys.argv[1], 'r')
     except FileNotFoundError as e:
         print(f"File Not Found Error: {e}")
 
